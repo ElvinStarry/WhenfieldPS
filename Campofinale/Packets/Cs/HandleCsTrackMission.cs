9621cf82744184f16ee7b8ee4b437ed4bbd10f07
Hexafluorine <guestuserboy@gmail.com>
2025-05-17 12:28:10 -0700

    Track Missions

---
+++ Campofinale/Packets/Cs/HandleCsTrackMission.cs
@@ -0,0 +1,20 @@
+﻿using Campofinale.Network;
+using Campofinale.Protocol;
+
+namespace Campofinale.Packets.Cs
+{
+    internal class HandleCsTrackMission
+    {
+        [Server.Handler(CsMsgId.CsTrackMission)]
+        public static void Handle(Player session, CsMsgId msgId, Packet packet)
+        {
+            CsTrackMission req = packet.DecodeBody<CsTrackMission>();
+
+            ScTrackMissionChange rsp = new()
+            {
+                MissionId = req.MissionId
+            };
+            session.Send(ScMsgId.ScTrackMissionChange, rsp);
+        }
+    }
+}
