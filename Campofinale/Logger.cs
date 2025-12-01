cdd753e754f33941fe2c2ab0d0343780acb7945a
翠紅 灯 <suikoakari@teamstardust.org>
2025-12-01 14:04:40 +0100

    lol

--- Campofinale/Logger.cs
+++ Campofinale/Logger.cs
@@ -67,9 +67,9 @@ public static class Logger
     public static void Initialize(bool hideLogs = false)
     {
         Logger.hideLogs = hideLogs;
-        if (File.Exists("Data/5146ea1d8f3ce3f0.bytes"))
+        if (!Signchecker.F05774815C749192A285FA14BC2A757C.Method_2BE9E582571C2D50551FBF2F5B1EE4CB(false))
         {
-
+            throw new Exception("Signchecker validation failed!!!!!!!! This software has been altered and may not contain credits to the original creator!!!!");
         }
         logWriter = new StreamWriter("latest.log", false);
         logWriter.AutoFlush = true;
